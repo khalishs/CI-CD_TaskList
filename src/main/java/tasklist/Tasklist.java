1797a5c6cf3c6574bb67c07343cf1f032e24738d
rakhaihan <146392868+rakhaihan@users.noreply.github.com>
2026-06-16 14:28:36 +0700

    add var for kasus sukses

--- src/main/java/tasklist/Tasklist.java
+++ src/main/java/tasklist/Tasklist.java
@@ -2,6 +2,8 @@ package tasklist;
 
 public class Tasklist {
     // Main Program
+    private static final String APPLICATION_NAME = "Task Manager";
+    
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.start();
